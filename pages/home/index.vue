83ffb21e1f9a97cf08d73cebf8609917cc191956
wxl <1814018727@qq.com>
2020-10-07 19:22:58 +0800

    'test1'

--- pages/home/index.vue
+++ pages/home/index.vue
@@ -3,7 +3,7 @@
         <div class="home-page">
             <div class="banner">
                 <div class="container">
-                    <h1 class="logo-font">Hello Everyone</h1>
+                    <h1 class="logo-font">动次打次，动次打次，呦呦~</h1>
                     <p>A place to share your knowledge.</p>
                 </div>
             </div>
